b4e694ea22f95a6fb236cba61819f1bf4915737c
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2014-04-16 14:55:07 +0200

    Use shared network access manager for children

--- native/webpage.cpp
+++ native/webpage.cpp
@@ -9,6 +9,7 @@ QWebPage *WebPage::createWindow(WebWindowType type)
     Q_UNUSED(type);
 
     WebPage *page = new WebPage(parent());
+    page->setNetworkAccessManager(networkAccessManager());
     emit windowCreated(page);
 
     return page;
