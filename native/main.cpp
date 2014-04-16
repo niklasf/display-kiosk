57b4e7bca573c45f15e35fe48edcf7993d32d66c
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2014-04-16 19:00:30 +0200

    Add application meta data

--- native/main.cpp
+++ native/main.cpp
@@ -5,11 +5,15 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setApplicationName("display-kiosk");
+    app.setApplicationVersion("0.3.0");
 
     Kiosk kiosk;
     kiosk.setResetText("hello");
     kiosk.setUrl(QUrl("http://example.com"));
     kiosk.setPreventClose(true);
+    kiosk.setAutoScroll(true);
+    kiosk.setAutoReload(3);
     kiosk.show();
 
     return app.exec();
